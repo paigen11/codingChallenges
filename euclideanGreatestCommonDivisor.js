3bc69be739399f8b6a3512ae3750996fa8200eff
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-08-02 16:07:53 -0400

    fixing logic

--- euclideanGreatestCommonDivisor.js
+++ euclideanGreatestCommonDivisor.js
@@ -9,23 +9,17 @@ function euclidGCD(a, b){
 } */
 
 function euclideanGCD(num1, num2){
-
-    var remainder = 0;
-    var gcd = 0;
-
     if(num1 === null || num2 === null){
         throw new Error('variables cannot be null');
     }
 
-    if(num2 == 0){
+    if(num2 === 0){
         console.log(num1);
         gcd = num1;
         return gcd;
     } else {
-        remainder = num1 % num2;
-        euclideanGCD(num2, remainder);
+        return euclideanGCD(num2, num1 % num2);
     }
-    return gcd;
 }
 
 euclideanGCD(3918848, 1653264); //61232
